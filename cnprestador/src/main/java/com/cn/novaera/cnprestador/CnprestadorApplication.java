c62940323ca75a2610e8c2ed28c30281c1bd185e
Willyan Fernando <Wff.cirino@gmail.com>
2022-07-18 13:28:04 -0300

    Adicionando o projeto

---
+++ cnprestador/src/main/java/com/cn/novaera/cnprestador/CnprestadorApplication.java
@@ -0,0 +1,13 @@
+package com.cn.novaera.cnprestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnprestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnprestadorApplication.class, args);
+	}
+
+}
---
+++ cnprestador/src/test/java/com/cn/novaera/cnprestador/CnprestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.novaera.cnprestador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnprestadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
